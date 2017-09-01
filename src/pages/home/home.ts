95b714cc80cb22c9602c15252db64f113a874653
faraznaeem <faraznaeem@gmail.com>
2017-09-01 09:59:45 +0200

    Add Geolocation to home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation';
+import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation';
 
 declare var google;
 
@@ -8,7 +8,8 @@ declare var google;
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  options : GeolocationOptions;
+  currentPos : Geoposition;
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   constructor(
@@ -29,12 +30,29 @@ export class HomePage {
       zoom: 13,
       center: {lat: 59.329323, lng: 18.068581}
     });
-    
+
     let watch = this.geolocation.watchPosition();
     watch.subscribe((data) => {
      // data can be a set of coordinates, or an error (if an error occurred).
      // data.coords.latitude
      // data.coords.longitude
    });
+  }
+  addMarker(){
+
+      let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+      });
+
+      let content = "<p>This is your current position !</p>";
+      let infoWindow = new google.maps.InfoWindow({
+      content: content
+      });
+
+      google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+      });
   }
 }
